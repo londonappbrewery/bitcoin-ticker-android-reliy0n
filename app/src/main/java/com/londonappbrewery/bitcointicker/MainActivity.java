736b36b5d68a321a6f32a2dbaf422daa6e981062
reliy0n <151805027@stu.adu.edu.tr>
2017-07-17 15:46:50 +0300

    a

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -14,66 +14,73 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.loopj.android.http.*;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    // Constants:
-    // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    Spinner spinner;
+    TextView update;
 
-    // Member Variables:
-    TextView mPriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.currency_spinner);
+        update  = (TextView) findViewById(R.id.priceLabel);
 
-        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
-        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = spinner.getSelectedItem().toString();
+                System.out.println(text);
+                update(text);
+            }
 
-        // Create an ArrayAdapter using the String array and a spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.currency_array, R.layout.spinner_item);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
-        // TODO: Set an OnItemSelected listener on the spinner
-
-    }
-
-    // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
-
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+            }
+        });
 
 
     }
 
+    // TODO: MY http code
+
+    private void update(String MONEY) {
+        update.setText("Updating ...");
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        String url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"+MONEY;
+        client.get(url, params, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject res) {
+                        // called when response HTTP status is "200 OK"
+                        try {
+                            String a = (String) res.getString("ask");
+                            update.setText(a);
+                        } catch (JSONException e) {
+                            update.setText("There was an error");
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                        update.setText("There was an error");
+                    }
+                }
+        );
+    }
+
+//https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD
 
 }
+
+
